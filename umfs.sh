2cb41b969fe6f542bfbc1912cf7dfdbca9001f76
Elian Adolfo Argañaraz Velazco <211253777+developerelianaav@users.noreply.github.com>
2025-07-02 13:07:43 -0300

    umfs v0.7

--- umfs.sh
+++ umfs.sh
@@ -1,42 +1,58 @@
 #!/bin/bash
 
-set -euo pipefail # Bash options to stop script on error (See set manpage).
-shopt -s extglob # Korn shell globbing.
+set -euo pipefail # Opción de Bash - Para el script apenas hay un error.
+shopt -s extglob # Englobamiento extendido de Ksh
 
-house='/home/estudiante' # User home.
+house='/home/estudiante' # El directorio base del usuario común.
 
 clean(){
+	# Se limpia con bleachbit los directorios/espacios comunes con archivos basura
 	bleachbit --clean firefox.* google_chrome.* \
 		libreoffice.* system.cache system.clipboard \
 		system.recent_documents system.tmp system.trash \
 		&>/dev/null
-	cd $house && rm -rf !(Clases|.config|.local) &>/dev/null
+	# Se elimina con rm todo excepto las tres carpetas
+	cd ${house} && rm -rf !(Clases|.config|.local) &>/dev/null
+	# Se recrean los directorios básicos del usuario
 	xdg-user-dirs-update &>/dev/null ; xdg-user-dirs-update --force  &>/dev/null
-	[ -d $house/Clases ] || mkdir -p $house/Clases && tar -C $house/Clases -xf $house/.config/clases.tar.gz &>/dev/null
-	[ -z "(ls $house/Clases)" ] && [ -f $house/.config/clases.tar.gz ] && \
-		tar -C $house/Clases -xf $house/.config/clases.tar.gz &>/dev/null
-	[ -f $house/Clases/.wall.png ] || wget -O $house/Clases/.wall.png "https://github.com/developerelianaav/scripts/blob/main/wall.png?raw=true" &>/dev/null
-	gsettings set org.gnome.desktop.background picture-uri file:///$house/Clases/.wall.png
-	[ -d $house/Clases ] && [ ! -z "(ls -A $house/Clases)" ] && tar -C $house/Clases -czf $house/.config/clases.tar.gz ./
+	# Se verifíca si el usuario eliminó el directorio Clases,
+	# En dado caso que si, se recrea.
+	[ -d ${house}/Clases ] || mkdir -p ${house}/Clases &>/dev/null
+	# Se verifica si existe el fondo de escritorio, en dado caso que no,
+	# Se descarga y se pone en la carpeta Clases.
+	[ -f ${house}/Clases/.wall.png ] || wget -O ${house}/Clases/.wall.png "https://github.com/developerelianaav/scripts/blob/main/wall.png?raw=true" &>/dev/null
+	# Se establece el fondo de escritorio.
+	gsettings set org.gnome.desktop.background picture-uri file:///${house}/Clases/.wall.png
 }
 
 
 install-prerequisites(){
-	cp $0 /usr/local/bin/umfs.sh # Copy this script.
-	chmod 755 /usr/local/bin/umfs.sh # The Script can't be read by the User, and can't be modified inside the system.
-	echo "/usr/local/bin/umfs.sh -c" >> $house/.profile # Add script to login.
-	sudo chown -R estudiante $house/.profile ; sudo chattr +i $house/.profile
-	sudo chown -R estudiante $house/.bashrc ; sudo chattr +i $house/.bashrc
-	[ -d $house/Clases ] || mkdir -p $house/Clases ; sudo chown -R estudiante $house/Clases
-	[ -d $house/Clases ] || wget -O $house/Clases/.wall.png "https://github.com/developerelianaav/scripts/blob/main/wall.png?raw=true"
+	# Copia el script, basicamente lo instala.
+	cp "${0}" /usr/local/bin/umfs.sh
+	# Cambia los permisos del script.
+	chmod 755 /usr/local/bin/umfs.sh
+	# Añade la siguiente línea a .profile, haciendo que la rutina de limpieza se ejecute al iniciar sesión.
+	echo "/usr/local/bin/umfs.sh -c" >> ${house}/.profile
+	# Se vuelve inmutable .profile y .bashrc
+	sudo chown -R estudiante ${house}/.profile ; sudo chattr +i ${house}/.profile
+	sudo chown -R estudiante ${house}/.bashrc ; sudo chattr +i ${house}/.bashrc
+	# Se crea la carpeta Clases.
+	[ -d ${house}/Clases ] || mkdir -p ${house}/Clases ; sudo chown -R estudiante ${house}/Clases
+	# Se descarga el fondo de pantalla.
+	[ -d ${house}/Clases ] || wget -O ${house}/Clases/.wall.png "https://github.com/developerelianaav/scripts/blob/main/wall.png?raw=true"
+	# Se instalan los programas (incompleto por ahora).
 	sudo apt install -y bleachbit xdg-user-dirs \
-		cron libreoffice build-essential jq 
+		cron libreoffice build-essential jq neovim \
+		vim xterm uxterm git nodejs npm swi-prolog \
+		mysql-server dia mongodb
 	echo -e "\033[0;32mDone\033[0m"
 }
 
 
 protect(){
+	# Se elimina al estudiante de los grupos peligrosos.
 	sed -i -e '/^\(root\|wheel\|sudo\)/{s/\(estudiante\|,estudiante\|estudiante,\)//g}' /etc/group /etc/gshadow
+	# Se le prohibe al usuario utilizar su.
 	sed -i -e 's/# auth       required   pam_wheel.so/auth       required   pam_wheel.so/g' /etc/pam.d/su
 	echo -e "\033[0;32mDone\033[0m"	
 }
@@ -55,13 +71,9 @@ shelp() {
 	echo "    Prints the version of this script"
 }
 
-update() {
-	echo "Checking for updates"
-}
-
 version() {
 	echo "umfs - UNLA's Multi Function Script"
-	echo "    Version 0.6"
+	echo "    Version 0.7"
 	echo "    Brougth to you by"
 	echo "    Unpayed Undergrads at UNLA"
 	echo "    License"
@@ -71,16 +83,16 @@ version() {
 while getopts "cihpv" option; do
 	case $option in
 		c)
-			clean # This option is meant to be used as a cronjob, Group 1 function.
+			clean # Grupo 2
 			;;
 		i)
-			install-prerequisites # One time only, Group 1 function.
+			install-prerequisites # Grupo 1
 			;;
 		h)
 			shelp
 			;;
 		p)
-			protect # One time only. Group 2 function.
+			protect # Grupo 2
 			;;
 		v)
 			version
