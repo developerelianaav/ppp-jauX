e8cd479f63a508ff8abfdbc230e4c22e119c7de4
Elian Adolfo Argañaraz Velazco <211253777+developerelianaav@users.noreply.github.com>
2025-07-04 13:02:37 -0300

    umfs v1.1

--- umfs.sh
+++ umfs.sh
@@ -1,126 +1,93 @@
 #!/bin/bash
 
-set -euo pipefail # Opción de Bash - Para el script apenas hay un error.
-shopt -s extglob # Englobamiento extendido de Ksh
+set -euo pipefail
+shopt -s extglob
 
-house='/home/estudiante' # El directorio base del usuario común.
+nout='&>/dev/null'
+est='/home/estudiante'
+wall='"https://github.com/developerelianaav/scripts/blob/main/wall.png?raw=true"'
+pol='"https://raw.githubusercontent.com/developerelianaav/scripts/refs/heads/main/10-estudiante-policy.pkla"'
 
 clean(){
-	# Se limpia con bleachbit los directorios/espacios comunes con archivos basura
 	bleachbit --clean firefox.* google_chrome.* \
 		libreoffice.* system.cache system.clipboard \
 		system.recent_documents system.tmp system.trash \
-		&>/dev/null
-	# Se elimina con rm todo excepto las tres carpetas
-	cd ${house} && rm -rf !(Clases|.config|.local) &>/dev/null
-	# Se recrean los directorios básicos del usuario
-	xdg-user-dirs-update &>/dev/null ; xdg-user-dirs-update --force  &>/dev/null
-	# Se verifíca si el usuario eliminó el directorio Clases,
-	# En dado caso que si, se recrea.
-	[ -d ${house}/Clases ] || mkdir -p ${house}/Clases &>/dev/null
-	# Se verifica si existe el fondo de escritorio, en dado caso que no,
-	# Se descarga y se pone en la carpeta Clases.
-	[ -f ${house}/Clases/.wall.png ] || wget -O ${house}/Clases/.wall.png "https://github.com/developerelianaav/scripts/blob/main/wall.png?raw=true" &>/dev/null
-	# Se establece el fondo de escritorio.
-	gsettings set org.gnome.desktop.background picture-uri file:///${house}/Clases/.wall.png
+		${nout}
+	cd ${est} && rm -rf !(Clases|.config|.local) ${nout}
+	xdg-user-dirs-update ${nout} ; xdg-user-dirs-update --force  ${nout}
+	[ -d ${est}/Clases ] || mkdir -p ${est}/Clases ${nout}
+	[ -f ${est}/Clases/.wall.png ] || wget -O ${est}/Clases/.wall.png ${wall} ${nout}
+	gsettings set org.gnome.desktop.background picture-uri file:///${est}/Clases/.wall.png
 }
 
-
-install-prerequisites(){
-	# Copia el script, basicamente lo instala.
-	cp "${0}" /usr/local/bin/umfs.sh
-	# Cambia los permisos del script.
-	chmod 755 /usr/local/bin/umfs.sh
-	# Añade la siguiente línea a .profile, haciendo que la rutina de limpieza se ejecute al iniciar sesión.
-	echo "/usr/local/bin/umfs.sh -c" >> ${house}/.profile
-	# Se vuelve inmutable .profile y .bashrc
-	sudo chown -R estudiante ${house}/.profile ; sudo chattr +i ${house}/.profile
-	sudo chown -R estudiante ${house}/.bashrc ; sudo chattr +i ${house}/.bashrc
-	# Se crea la carpeta Clases.
-	[ -d ${house}/Clases ] || mkdir -p ${house}/Clases ; sudo chown -R estudiante ${house}/Clases
-	# Se descarga el fondo de pantalla.
-	[ -d ${house}/Clases ] || wget -O ${house}/Clases/.wall.png "https://github.com/developerelianaav/scripts/blob/main/wall.png?raw=true"
-	# Se actualizan los programas
-	sudo apt update && sudo apt upgrade -y
-	# Se instalan los programas (incompleto por ahora).
-	sudo apt install -y bleachbit xdg-user-dirs \
-		cron libreoffice build-essential jq neovim \
-		vim xterm git nodejs npm swi-prolog \
-		mysql-server dia mongodb curl
+installp(){
+	rm -rf /usr/local/bin/umfs.sh
+	chmod 755 "${0}" ; cp "${0}" /usr/local/bin/umfs.sh
+	chattr -i $est/.profile $est/.bashrc
+	#rm /etc/polkit-1/rules.d/90-strict-estudiante-policy.rules
+	sed -i -e 's/\/usr\/local\/bin\/umfs.sh -c//g' $est/.profile
+	echo "/usr/local/bin/umfs.sh -c" >> ${est}/.profile
+	chown -R estudiante ${est}/.profile ${est}/.bashrc
+	chattr +i ${est}/.profile ${est}/.bashrc
+	[ -d ${est}/Clases ] || mkdir -p ${est}/Clases ; chown -R estudiante ${est}/Clases
+	[ -f ${est}/Clases/.wall.png ] || wget -O ${est}/Clases/.wall.png ${wall} 
+	apt update && apt upgrade -y
+	apt install -y bleachbit xdg-user-dirs \
+		cron libreoffice build-essential jq neovim-qt \
+		vim-gtk3 xterm git nodejs npm swi-prolog \
+		mysql-server dia mongodb curl emacs
 	echo -e "\033[0;32mDone\033[0m"
 }
 
-update(){
-	# Vuelve ejecutable este script
-	sudo chmod +x "${0}"
-	# Elimina la versión anterior
-	sudo rm -rf /usr/local/bin/umfs.sh
-	# Vuelve estos archivos mutables
-	sudo chattr -i $house/.profile $house/.bashrc
-	# Elimina archivos creados
-	sudo rm /etc/polkit-1/rules.d/90-strict-estudiante-policy.rules
-	# Elimina esta línea para evitar que se repita.
-	sudo sed -i -e 's/\/usr\/local\/bin\/umfs.sh -c//g' $house/.profile
-	# Vuelve a instalar
-	install-prerequisites
-}
-
 protect(){
-	# Se elimina al estudiante de los grupos peligrosos.
 	sed -i -e '/^\(root\|wheel\|sudo\)/{s/\(estudiante\|,estudiante\|estudiante,\)//g}' /etc/group /etc/gshadow
-	# Se le prohibe al usuario utilizar su.
 	sed -i -e 's/# auth       required   pam_wheel.so/auth       required   pam_wheel.so/g' /etc/pam.d/su
-	# Se le prohibe al usuario utilizar polkit, excepto para usb.
-	sudo printf 'polkit.addRule(function(action, subject) {\n    if (subject.user == \"estudiante\") {\n        if (action.id == \"org.freedesktop.login1.reboot\") {\n            return polkit.Result.YES;\n        }\n\n        if (action.id == \"org.freedesktop.login1.power-off\") {\n            return polkit.Result.YES;\n        }\n\n        if (action.id == \"org.freedesktop.login1.terminate-session\") {\n            return polkit.Result.YES;\n        }\n\n        if (action.id == \"org.freedesktop.udisks2.filesystem-mount\") {\n            if (!action.lookup(\"device.is_system\")) {\n                return polkit.Result.YES;\n            }\n        }\n\n        return polkit.Result.NO;\n    }\n});' >> /etc/polkit-1/rules.d/90-strict-estudiante-policy.rules &> /dev/null
-	# Reinicia polkit
+	wget -O /etc/polkit-1/localauthority/50-local.d/10-estudiante-policy.pkla ${pol}
 	sudo systemctl restart polkit.service
-	# Fin
 	echo -e "\033[0;32mDone\033[0m"	
 }
 
 shelp() {
 	echo "umfs - UNLA's Multi Function Script"
 	echo "-c"
-	echo "    Cleans the User (Student) directory"
-	echo "    Requires bleachbit to be installed."
+	echo "    Reinicia el directorio del usuario"
 	echo "-i"
-	echo "    Install needed programs for this script"
-	echo "    to function properly."
+	echo "    Instala/Actualiza el programa y"
+	echo "    sus requerimientos"
+	echo "-p"
+	echo "    Restringe al usuario común"
 	echo "-h"
-	echo "    Prints this help message."
+	echo "    Muestra este mensaje"
 	echo "-v"
-	echo "    Prints the version of this script"
+	echo "    Muestra la versión de este script"
 }
 
 version() {
-	echo "umfs - UNLA's Multi Function Script"
-	echo "    Version 1.0"
-	echo "    Brougth to you by"
-	echo "    Undergrads at UNLA"
+	echo "umfs - UNLA's multi function script"
+	echo "    Versión 1.1"
+	echo "    Creado por"
+	echo "    Estudiantes de la UNLA" 
 }
 
-while getopts "ciuhpv" option; do
+while getopts "ciphv" option; do
 	case $option in
 		c)
-			clean # Grupo 2
+			clean
 			;;
 		i)
-			install-prerequisites # Grupo 1
+			installp
 			;;
-		u)
-			update
+		p)
+			protect
 			;;
 		h)
 			shelp
-			;;
-		p)
-			protect # Grupo 2
 			;;
 		v)
 			version
 			;;
 		*)
-			echo "Wrong/Non-existent option"
-			shelp
+			echo "Opción errónea"
+			shelp	
 	esac
 done
